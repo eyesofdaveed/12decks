74c0a03582dace2f67fa4bf55dba73b520141a24
eyesofdaveed <davdauit@gmail.com>
2025-03-01 13:05:49 +0500

    add tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,6 +22,52 @@ function MyApp({ Component, pageProps }) {
           name="description"
           content="Decking and Fencing Design Installation Repair Services in Greater Seattle Area"
         />
+        {/* Google Tag Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-710701541"
+        ></script>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'AW-710701541');
+      `,
+          }}
+        />
+
+        {/* Yandex Metrika Script */}
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+         
+            ym(99860187, "init", {
+                 clickmap:true,
+                 trackLinks:true,
+                 accurateTrackBounce:true,
+                 webvisor:true
+            });`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/99860187"
+              style="position:absolute; left:-9999px;"
+              alt=""
+            />
+          </div>
+        </noscript>
+
         {/* seo end */}
       </Head>
       <Component {...pageProps} />
